2e545049a6733be4ab5d9ac677e343d9b4532ac8
Neil Stephens <dearknarl@gmail.com>
2026-05-04 14:46:38 +1000

    Add header to syscall lib code and compile with -O3

--- Examples/TinyElfSysCall.c
+++ Examples/TinyElfSysCall.c
@@ -1,3 +1,39 @@
+/*
+ *
+ * https://github.com/neilstephens/TinyRISCV64
+ *
+ * MIT License
+ *
+ * Copyright (c) 2025 Neil Stephens
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * This example code shows how the ecall ABI in TinyElfRISCV64.h can be used.
+ * This code was writen for use with picolibc - it implements the set of OS functions that picolibc needs to compile ISO C code
+ *   see: https://github.com/picolibc/picolibc/blob/main/doc/os.md
+ *
+ * a pre-compiled static library of this code is provided, so you can compile like:
+ *    Examples$ riscv64-unknown-elf-gcc -L. --oslib=TinyElfSysCall -march=rv64im -mabi=lp64 -nostartfiles -static -T vm.ld -O3 YourCode.c -o YourBin
+ */
+
 #include <errno.h>
 #include <fcntl.h>
 #include <signal.h>
